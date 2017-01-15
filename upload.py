6b1c88aebb8efcd6ccd6fea996f102a657a91e40
Timothy Spann <tspann@hortonworks.com>
2017-01-15 12:39:32 -0500

    Update upload.py

--- upload.py
+++ upload.py
@@ -1,36 +1,59 @@
 #!/usr/bin/python
 
+import os
+import datetime
+import ftplib
+import traceback
 import math
 import random, string
 import base64
 import json
-import paho.mqtt.client as paho
+import paho.mqtt.client as mqtt
 import picamera
 from time import sleep
+from time import gmtime, strftime
 
 packet_size=3000
 
 def randomword(length):
  return ''.join(random.choice(string.lowercase) for i in range(length))
  
-def convertImageToBase64():
- with open("image_test.jpg", "rb") as image_file:
- encoded = base64.b64encode(image_file.read())
- return encoded
+
+# Create unique image name
+img_name = 'pi_image_{0}_{1}.jpg'.format(randomword(3),strftime("%Y%m%d%H%M%S",gmtime()))
  
+# Capture Image from Pi Camera
 try: 
  camera = picamera.PiCamera()
- camera.capture('image_test.jpg', resize=(500,281))
- camera.annotate_background = Color('blue')
- camera.annotate_foreground = Color('yellow')
- camera.annotate_text = " Apache NiFi "
+ camera.annotate_text = " Stored with Apache NiFi "
+ camera.capture(img_name, resize=(500,281))
 
  pass
 finally:
  camera.close()
  
-json_string = json.dumps(row)
-client = paho.Client()
-client.username_pw_set("username","password")
-client.connect("server.cloudmqtt.com", 14162, 60)
-client.publish("sensor", payload=json_string, qos=0, retain=True)
+# MQTT
+client = mqtt.Client()
+client.username_pw_set("CloudMqttUserName","!MakeSureYouHaveAV@5&L0N6Pa55W0$4!")
+client.connect("cloudmqttiothoster", 14162, 60)
+
+f=open(img_name)
+fileContent = f.read()
+byteArr = bytearray(fileContent)
+f.close()
+message = '"image": {"bytearray":"' + byteArr + '"} } '
+print client.publish("image",payload=message,qos=1,retain=False)
+client.disconnect()
+
+# FTP
+ftp = ftplib.FTP()
+ftp.connect("ftpserver", "21")
+try:
+    ftp.login("reallyLongUserName", "FTP PASSWORDS SHOULD BE HARD")
+    ftp.storbinary('STOR ' + img_name, open(img_name, 'rb'))
+finally:
+    ftp.quit()
+
+# clean up sent file
+os.remove(img_name)
+
